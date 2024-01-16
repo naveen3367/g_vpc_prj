79174e8d50e6ae7125a33272ac86d38767c5af62
Nav <naveen3367@gmail.com>
2024-01-16 01:13:57 -0500

    prod commit

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.17.0"
     }
   }
+
+  backend "s3" {
+    bucket = "mybucket"
+    key    = "path/to/my/key"
+    region = "us-east-1"
+  }
+  
 }
 
 provider "aws" {
